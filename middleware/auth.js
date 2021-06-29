7efe0e786c7358388de3c942d054b5a0d85b3a9a
Gregoire <gregoire.p.langlois@gmail.com>
2021-06-29 12:18:11 +0200

    Test commit with gitignore

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,15 +3,12 @@
 // Importation du module "Jsonwebtoken".
 const jwt = require('jsonwebtoken');
 
-const SECRET_KEY = process.env.SECRET_KEY;
-
 module.exports = (req, res, next) => {
   // Compte tenu des potentiels problème de ce middleware, on englobe le code entre les fonctions "Try" et "Catch"
   try {
     // Extraction du Token propre au header "authorization" Utilisation de split pour récupérer tout après l'espace dans le header. 
     //Les erreurs générées ici s'afficheront dans le bloc catch ;
     const token = req.headers.authorization.split(' ')[1];
-    console.log(token);
     // Utilisation de la fonction verify pour décoder notre token. Si celui-ci n'est pas valide, une erreur sera générée ;
     const decodedToken = jwt.verify(token, process.env.SECRET_KEY);
     // Extraction de l'ID utilisateur du Token
